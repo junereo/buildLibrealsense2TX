50852f9431ff319aa0bfd8ddd25808c7b5c58b8b
junereo <55540748+junereo@users.noreply.github.com>
2020-01-02 14:26:09 +0900

    Update patchKernel.sh

--- scripts/patchKernel.sh
+++ scripts/patchKernel.sh
@@ -25,11 +25,11 @@ ubuntu_codename=`. /etc/os-release; echo ${UBUNTU_CODENAME/*, /}`
 # ubuntu_codename is xenial for L4T 28.X (Ubuntu 16.04)
 # Patching kernel for RealSense devices
 echo -e "\e[32mApplying realsense-uvc patch\e[0m"
-patch -p1 < ${LIBREALSENSE_DIR}/scripts/realsense-camera-formats_ubuntu-${ubuntu_codename}-${kernel_branch}.patch 
+patch -p1 < ${LIBREALSENSE_DIR}/scripts/realsense-camera-formats-${ubuntu_codename}-${kernel_branch}.patch 
 echo -e "\e[32mApplying realsense-metadata patch\e[0m"
-patch -p1 < ${LIBREALSENSE_DIR}/scripts/realsense-metadata-ubuntu-${ubuntu_codename}-${kernel_branch}.patch
+patch -p1 < ${LIBREALSENSE_DIR}/scripts/realsense-metadata-${ubuntu_codename}-${kernel_branch}.patch
 echo -e "\e[32mApplying realsense-hid patch\e[0m"
-patch -p1 < ${LIBREALSENSE_DIR}/scripts/realsense-hid-ubuntu-${ubuntu_codename}-${kernel_branch}.patch
+patch -p1 < ${LIBREALSENSE_DIR}/scripts/realsense-hid-${ubuntu_codename}-${kernel_branch}.patch
 echo -e "\e[32mpowerlinefrequency-control-fix patch\e[0m"
 patch -p1 < ${LIBREALSENSE_DIR}/scripts/realsense-powerlinefrequency-control-fix.patch
 
